7b9423beebc39a7f7cb0b4ecd81fe84227e39b4d
GitHub Action <action@github.com>
2024-12-19 19:53:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/contactanuragvardhan/intellijplatformplugintest/MyBundle.kt
+++ src/main/kotlin/com/github/contactanuragvardhan/intellijplatformplugintest/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.contactanuragvardhan.intellijplatformplugintest
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
